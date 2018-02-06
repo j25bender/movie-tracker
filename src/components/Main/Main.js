b06367fdf350e1a510f3a0527a381b0670590e07
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-06 13:03:16 -0700

    Make Main a container and almost successfully fetch data

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../Card/Card';
+import movieData from '../../helpers/helper.js';
+import { connect } from 'react-redux';
+import { getMovies } from '../../actions/index.js'
 import './Main.css';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-const Main = ({ movieData }) => {
-  if (movieData.length) {
-    const movies = movieData.map(movie => {
-      return <Card movieData={movie} key={movie.id} />;
-    });
-    return <div className="main">{movies}</div>;
-  } else {
-    return null;
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   movieData: []
+    // };
+  }
+
+  componentDidMount = async () => {
+    const fetchedMovieData = await movieData.fetchMovies();
+    console.log(fetchedMovieData)
+    this.props.fetchMovies(fetchedMovieData);
+    // this.setState({ movieData: fetchedMovieData });
+  };
+
+  render() {
+    console.log(this.props)
+    const movieData = this.props.movieData;
+    if (movieData.length) {
+      const movies = movieData.map(movie => {
+        return <Card movieData={movie} key={movie.id} />;
+      });
+      return <div className="main">{movies}</div>;
+    } else {
+      return null;
+    }
   }
 };
 
-Main.propTypes = {
-  movieData: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-      poster: PropTypes.string.isRequired
-    })
-  )
-};
+export const mapStateToProps = store => ({
+  movieData: store.movieData || []
+});
 
-export default Main;
+export const mapDispatchToProps = dispatch => ({
+  fetchMovies: movieData => dispatch(getMovies(movieData))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
+
+// Main.propTypes = {
+//   movieData: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       title: PropTypes.string.isRequired,
+//       id: PropTypes.number.isRequired,
+//       poster: PropTypes.string.isRequired
+//     })
+//   )
+// };
+
+// export default Main;
