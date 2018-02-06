236be6ade8bb9cf9331c77d1a7c3d19dcc355b66
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-06 13:04:41 -0700

    Add provider, createStore, and rootReducer to index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index.js';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render((
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>), document.getElementById('root'));
 registerServiceWorker();
