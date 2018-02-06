7d2665a1b292ee21e36b24815707ef983249d864
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-06 07:53:35 -0700

    Add BrowserRouter to index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-// import { apiKey } from './assets/apiKey'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>), document.getElementById('root'));
 registerServiceWorker();
