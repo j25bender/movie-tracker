105bfea0b74ed4c24210897a966f1b1c948891e3
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-05 20:05:36 -0700

    Add initial fetch to componentDidMount, have App render header and main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import Main from '../Main/Main';
+import Header from '../Header/Header';
+import movieData from '../../helpers/helper.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movieData: []
+    }
+  }
+
+  componentDidMount = async () => {
+    const fetchedMovieData = await movieData.fetchMovies();
+    this.setState({ movieData: fetchedMovieData });
+  }
+
   render() {
     return (
       <div className="App">
-        <Main />
+        <Header />
+        <Main movieData={ this.state.movieData } />
       </div>
     );
   }
