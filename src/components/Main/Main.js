8a5fc1ca8bd50c69c25a285f6bd15e25bc04f80c
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-05 20:14:25 -0700

    Add key prop of movie id to Card

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,11 +3,11 @@ import Card from '../Card/Card';
 import './Main.css'
 
 const Main = ({ movieData }) => {
-  console.log(movieData)
   if (movieData.length) {
     const movies = movieData.map( movie => {
       return (
-        <Card movieData={movie}/>
+        <Card movieData={movie}
+              key={movie.id} />
       )
     })
     return (
