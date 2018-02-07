dd732dfcedd579bf12179cde0313690c5fc701d2
Jeff Bender <j25bender@gmail.com>
2018-02-07 13:44:18 -0700

    added email to sign up and set up backend

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,8 +1,10 @@
 export const userReducer = (state = {}, action) => {
     switch(action.type) {
         case 'ADD_USER':
-            return { username: action.username,       
-                     password: action.password 
+            return { 
+                username: action.username,
+                email: action.email,       
+                password: action.password 
             }
         default: 
             return state
