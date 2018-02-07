334a4b6d66aa9ac7ea3bba63a93823eef1b80814
Spencer Herms <slherms@gmail.com>
2018-02-07 14:29:44 -0700

    verfies backend is talking to front end

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -4,7 +4,6 @@ import fetchApi from './apiCalls';
 const imageUrl = 'https://image.tmdb.org/t/p/w500';
 
 const cleanMovies = (movieData) => {
-  console.log(movieData)
   return movieData.results.map( movie => {
     return {
       title: movie.title,
@@ -19,6 +18,13 @@ const fetchMovies = async () => {
   const movieFetch = await fetchApi(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`);
   return cleanMovies(movieFetch);
 }
+
+const checkBackend = async () => {
+  const callApi = await fetchApi('/api/users')
+  console.log(callApi);
+}
+
+checkBackend()
 
 export default {
   fetchMovies
