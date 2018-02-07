88a0df65f522c32ce490b62c2713afd9d21b324d
Spencer Herms <slherms@gmail.com>
2018-02-07 14:29:44 -0700

    verfies backend is talking to front end

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import  { addUser } from '../../actions/index'
-import { connect } from 'react-redux'
-
+import { addUser } from '../../actions/index';
+import { connect } from 'react-redux';
 
 class SignUp extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       username: '',
       email: '',
       password: ''
-    }
+    };
   }
 
   render() {
@@ -19,30 +18,46 @@ class SignUp extends Component {
 
     return (
       <section>
-        <form onSubmit={ (e) => {
-              e.preventDefault()
-              handleSubmit(this.state.email,                                 this.state.password)
-        }}>
-        <input value={ this.state.username }
-                 placeholder='username' 
-                 onChange={ (e) => this.setState({ username: e.target.value })} />
-          <input value={ this.state.email }
-                 placeholder='email' 
-                 onChange={ (e) => this.setState({ email: e.target.value })} />
-          <input value={ this.state.password }
-                 placeholder='Password'
-                 type='password'
-                 onChange={ (e) => this.setState({ password: e.target.value })} />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            handleSubmit(
+              this.state.username,
+              this.state.email.toLowerCase(),
+              this.state.password
+            );
+          }}
+        >
+          <input
+            value={this.state.username}
+            placeholder="username"
+            onChange={e => this.setState({ username: e.target.value })}
+            required
+          />
+          <input
+            value={this.state.email}
+            placeholder="email"
+            type="email"
+            onChange={e => this.setState({ email: e.target.value })}
+            required
+          />
+          <input
+            value={this.state.password}
+            placeholder="Password"
+            type="password"
+            onChange={e => this.setState({ password: e.target.value })}
+            required
+          />
           <button>Sign Up</button>
         </form>
       </section>
-    )
+    );
   }
-
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  handleSubmit: (username, email, password) => dispatch(addUser(username, email, password))
-})
+const mapDispatchToProps = dispatch => ({
+  handleSubmit: (username, email, password) =>
+    dispatch(addUser(username, email, password))
+});
 
-export default connect(null, mapDispatchToProps)(SignUp)
+export default connect(null, mapDispatchToProps)(SignUp);
