b0432c3f2622bfd6ef5201b8d35e0744ae15aeae
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-07 07:49:24 -0700

    Change dispatch to call new thunk action creator

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,27 +2,21 @@ import React, { Component } from 'react';
 import Card from '../Card/Card';
 import movieData from '../../helpers/helper.js';
 import { connect } from 'react-redux';
-import { getMovies } from '../../actions/index.js'
+import { getMoviesFromApi } from '../../actions/index.js';
 import './Main.css';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Main extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   movieData: []
-    // };
   }
 
   componentDidMount = async () => {
     const fetchedMovieData = await movieData.fetchMovies();
-    console.log(fetchedMovieData)
     this.props.fetchMovies(fetchedMovieData);
-    // this.setState({ movieData: fetchedMovieData });
   };
 
   render() {
-    console.log(this.props)
     const movieData = this.props.movieData;
     if (movieData.length) {
       const movies = movieData.map(movie => {
@@ -33,32 +27,27 @@ class Main extends Component {
       return null;
     }
   }
-};
+}
 
 export const mapStateToProps = store => ({
   movieData: store.movieData || []
 });
 
-// export const mapDispatchToProps = dispatch => ({
-//   fetchMovies: movieData => dispatch(getMovies(movieData))
-// });
-
-export const mapDispatchToProps = dispatch => {
-  console.log(dispatch);
-  
-  return {fetchMovies: movieData => dispatch(getMovies(movieData))}
-};
+export const mapDispatchToProps = dispatch => ({
+  fetchMovies: movieData => dispatch(getMoviesFromApi(movieData))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
 
-// Main.propTypes = {
-//   movieData: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       title: PropTypes.string.isRequired,
-//       id: PropTypes.number.isRequired,
-//       poster: PropTypes.string.isRequired
-//     })
-//   )
-// };
+Main.propTypes = {
+  movieData: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      poster: PropTypes.string.isRequired
+    })
+  ),
+  fetchMovies: PropTypes.func
+};
 
 // export default Main;
