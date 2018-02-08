ebf7aff95c51b91f18b229c9e2a93f38cb2b9423
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-08 11:57:31 -0700

    Add fetchBackend to api calls

--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -7,9 +7,26 @@ const fetchApi = async url => {
       throw new Error('Bad staus code!');
     }
   } catch (error) {
-    const error = new Error('fetchApi failed to fetch data');
+    const error = new Error(`fetchApi failed to fetch data: ${error}`);
     return error;
   }
 };
 
-export default fetchApi;
+const fetchBackend = async url => {
+  try {
+    const initialFetch = await fetch(url);
+    if (initialFetch.status <= 200) {
+      return await initialFetch.json();
+    } else {
+      throw new Error('Bad staus code!');
+    }
+  } catch (error) {
+    const error = new Error(`fetchUser failed to fetch data: ${error}`);
+    return error;
+  }
+}
+
+export {
+  fetchApi,
+  fetchBackend
+};
