97453ac2638712f95997156fbda09a912e3f90c0
Spencer Herms <slherms@gmail.com>
2018-02-07 23:25:21 -0700

    creates user reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,12 +1,12 @@
 export const userReducer = (state = {}, action) => {
-    switch(action.type) {
-        case 'ADD_USER':
-            return { 
-                username: action.username,
-                email: action.email,       
-                password: action.password 
-            }
-        default: 
-            return state
-    }
-}
+  switch (action.type) {
+  case 'ADD_USER':
+    return {
+      username: action.username,
+      email: action.email,
+      password: action.password
+    };
+  default:
+    return state;
+  }
+};
