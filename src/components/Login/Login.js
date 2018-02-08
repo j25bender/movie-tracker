1588cbf191c0890e5067897dc387fb8702ebabf6
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-08 11:56:19 -0700

    Make form in login and make it a container

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import fetchUser from '../../helpers/helper';
+import { getUser } from '../../actions/index';
 
-const Login = () => {
-  return <div>I&#39;m a login</div>;
-};
+class Login extends Component {
+  constructor(props) {
+    super(props);
 
-export default Login;
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  async componentDidMount() {
+    console.log(fetchUser.fetchUser())
+  }
+
+  render() {
+    console.log('props',this.props)
+    // const { handleSubmit } = this.props;
+    return (
+      <section>
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            this.props.handleSubmit(
+              this.state.email.toLowerCase(),
+              this.state.password
+            );
+          }}
+        >
+          <input
+            value={this.state.email}
+            placeholder="email"
+            type="email"
+            autoComplete="email"
+            onChange={event => this.setState({ email: event.target.value })}
+            required
+          />
+          <input
+            value={this.state.password}
+            placeholder="password"
+            type="password"
+            autoComplete="on"
+            onChange={event => this.setState({ password: event.target.value })}
+            required
+          />
+          <button>Log In</button>
+        </form>
+      </section>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  handleSubmit: (email, password) => (
+    dispatch(getUser(email, password))
+  )
+}
+
+export default connect(null, mapDispatchToProps)(Login);
