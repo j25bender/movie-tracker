19a8eec0e0bffdba93fb317d6016a90d9dc02be4
Spencer Herms <slherms@gmail.com>
2018-02-08 22:38:21 -0700

    formats, adds eslint disable

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -6,5 +7,5 @@ import * as actions from '../index';
 it('should call action GET_Movies', () => {
   const mockgetMovies = jest.fn();
   actions.getMoviesFromApi();
-  expect(actions.getMovies()).toHaveBeenCalled()
-})
+  expect(actions.getMovies()).toHaveBeenCalled();
+});
