06b6fb744fe81638d335be9824c0f0a3ac19ac7a
Spencer Herms <slherms@gmail.com>
2018-02-10 14:31:32 -0700

    add toggleFavorite and post favorite

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,23 +4,63 @@ import { connect } from 'react-redux';
 import { getMoviesFromApi } from '../../actions/index.js';
 import './Main.css';
 import PropTypes from 'prop-types';
+import { fetchApi, postBackend } from '../../helpers/apiCalls';
 
 class Main extends Component {
-
   componentDidMount = async () => {
     this.props.fetchMovies();
+  };
+
+  postFavorite = async movieData => {
+    const { userId: user_id } = this.props;
+    
+    const {
+      id: movie_id,
+      title,
+      poster: poster_path,
+      release_date,
+      vote_average,
+      overview
+    } = movieData;
+
+    const favoriteProperties = {
+      movie_id,
+      user_id,
+      title,
+      poster_path,
+      release_date,
+      vote_average,
+      overview
+    };
+
+    const addFavorite = await postBackend('api/users/favorites/new/', favoriteProperties);
+  };
+
+  toggleFavorite = async movieData => {
+    const { userId } = this.props;
+    const existingFavorites = await fetchApi(`api/users/${userId}/favorites/`);
+    this.postFavorite(movieData);
   };
 
   render() {
     const { movieData, loggedIn } = this.props;
     if (movieData.length) {
       const movies = movieData.map(movie => {
-        return <Card movieData={ movie } key={ movie.id } loggedIn={ loggedIn } />;
+        return (
+          <Card
+            movieData={movie}
+            key={movie.id}
+            loggedIn={loggedIn}
+            toggleFavorite={this.toggleFavorite}
+          />
+        );
       });
-      return <div className="main">
-        <button className='view-favorites'>Favorites</button>
-        { movies }
-      </div>;
+      return (
+        <div className="main">
+          <button className="view-favorites">Favorites</button>
+          {movies}
+        </div>
+      );
     } else {
       return null;
     }
@@ -29,7 +69,8 @@ class Main extends Component {
 
 export const mapStateToProps = state => ({
   movieData: state.movieData || [],
-  loggedIn: state.loggedIn
+  loggedIn: state.loggedIn,
+  userId: state.userData.userId
 });
 
 export const mapDispatchToProps = dispatch => ({
@@ -46,6 +87,6 @@ Main.propTypes = {
       poster: PropTypes.string.isRequired
     }).isRequired
   ).isRequired,
-  
+
   fetchMovies: PropTypes.func
 };
