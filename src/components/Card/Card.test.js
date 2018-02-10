bbacffb64b447dc6eb294f066e18f3813c1d009c
Spencer Herms <slherms@gmail.com>
2018-02-09 17:53:50 -0700

    add eslint disable

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,21 +1,25 @@
+/* eslint-disable */
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Card from './Card';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Card', () => {
-    const  mockMovieData = {title: "Coco", 
-                            id: 354912, 
-                            poster: "https://image.tmdb.org/t/p/w500/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg", 
-                            overview: "Despite his family’s baffling generations-old ban …ck the real story behind Miguel's family history."
-                           };
+  const mockMovieData = {
+    title: 'Coco',
+    id: 354912,
+    poster: 'https://image.tmdb.org/t/p/w500/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
+    overview:
+      "Despite his family’s baffling generations-old ban …ck the real story behind Miguel's family history."
+  };
 
-    it('matches the snapshot', () => {
-        const wrapper = shallow(<Card movieData={mockMovieData} key={mockMovieData.id} />);
-        
-        expect(wrapper).toMatchSnapshot();
-    });
-    
+  it('matches the snapshot', () => {
+    const wrapper = shallow(
+      <Card movieData={mockMovieData} key={mockMovieData.id} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
 });
