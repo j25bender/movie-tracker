27494e891c6fdf0eb489ab86ab68994cd7306b9a
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-11 16:59:00 -0700

    Add almost all tests

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Login, mapDispatchToProps } from './Login';
-import { filteredMovieData } from '../../helpers/mockData';
+import { filteredMovieData, movieDataSansFavs } from '../../helpers/mockData';
 import { getUser, login } from '../../actions/index';
 import * as helper from '../../helpers/helper';
 import { createStore } from 'redux';
@@ -40,30 +40,69 @@ describe('Login', () => {
     });
 
     describe('validateLogin', () => {
-        it('should fetch if status is less than 200', async () => {
+        beforeEach(() => {
             window.fetch = jest.fn().mockImplementation( (url) => {
             return Promise.resolve({
                 status: 200,
                 json: () => Promise.resolve({ data: filteredMovieData })
+                })
             })
-        })
-            expect(window.fetch).not.toHaveBeenCalled();
             helper.fetchUser = jest.fn().mockImplementation(() => {
                 return window.fetch()
             })
             helper.fetchApi = jest.fn().mockImplementation(() => {
                 return { data: filteredMovieData };
             })
+        })
+
+        it('should fetch if status is less than 200', async () => {
             wrapper.instance().validateLogin();
             expect(window.fetch).toHaveBeenCalled();
         })
+
+        it('calls loadFavorites, handleLogin, and handleSubmit if user email and password match', async () => {
+            wrapper.instance().loadFavorites = jest.fn()
+            expect(wrapper.instance().props.handleLogin).not.toHaveBeenCalled();
+            expect(wrapper.instance().props.handleSubmit).not.toHaveBeenCalled();
+            expect(wrapper.instance().loadFavorites).not.toHaveBeenCalled();
+            
+            await wrapper.instance().validateLogin();
+
+            expect(wrapper.instance().props.handleLogin).toHaveBeenCalled();
+            expect(wrapper.instance().props.handleSubmit).toHaveBeenCalled();
+            expect(wrapper.instance().loadFavorites).toHaveBeenCalled();
+        })
     })
 
+    describe('markMoviesAsFavorites', () => {
+        it('adds a favorites property to all movies passed in', () => {
+            expect(wrapper.instance().markMoviesAsFavorites(movieDataSansFavs)[0])
+            .toEqual(filteredMovieData[0])
+        })
+    })
 
-    it.skip('should call validateLogin on submit', () => {
+    describe('markFavsAsFavorites', () => {
+        it('adds a favorites property to all movies passed in', () => {
+            expect(wrapper.instance().markFavsAsFavorites(movieDataSansFavs)[0])
+            .toEqual(filteredMovieData[0])
+        })
+    })
 
-        expect(window.fetch).toHaveBeenCalled()
-
+    describe('matchStateToProps', () => {
         
     })
+
+    describe('matchDispatchToProps', () => {
+        it('should call dispatch when a function prop is called', () => {
+          const mockDispatch = jest.fn()
+          const mapped = mapDispatchToProps(mockDispatch);
+          mapped.handleSubmit();
+          expect(mockDispatch).toHaveBeenCalled();
+          mapped.handleLogin();
+          mapped.setFavorites();
+          mapped.getMovies();
+          expect(mockDispatch).toHaveBeenCalledTimes(4);
+        })
+    })
+
 });
