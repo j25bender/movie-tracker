4cf4a5d033ba6cfc8d8d7ebb6722f5c92e92e2b9
Spencer Herms <slherms@gmail.com>
2018-02-10 21:05:47 -0700

    adds test basics

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import Card from '../Card/Card';
+import { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+
+//Enzyme.configure({ adapter: new Adapter() });
+
+describe('Main', () => {
+    it('matches the snapshot', () => {
+        // const wrapper = shallow(<Main />);
+        
+        // expect(wrapper).toMatchSnapshot();
+    }); 
+});
