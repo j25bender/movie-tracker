5ab5c9bb6c677520fd1c0597d724959f3d517212
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-10 17:27:12 -0700

    Delete unused console.log

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,7 +21,6 @@ export const getUser = (email, password, userId, name) => ({
   name
 });
 
-//could become toggleLogin
 export const login = boolean => ({
   type: 'LOGIN',
   boolean
@@ -44,23 +43,3 @@ export const getMoviesFromApi = () => {
     fetchMovies().then(movieData => dispatch(getMovies(movieData)));
   };
 };
-
-// export function itemsFetchData(url) {
-//     return (dispatch) => {
-//         dispatch(itemsIsLoading(true));
-
-//         fetch(url)
-//             .then((response) => {
-//                 if (!response.ok) {
-//                     throw Error(response.statusText);
-//                 }
-
-//                 dispatch(itemsIsLoading(false));
-
-//                 return response;
-//             })
-//             .then((response) => response.json())
-//             .then((items) => dispatch(itemsFetchDataSuccess(items)))
-//             .catch(() => dispatch(itemsHasErrored(true)));
-//     };
-// }
