ec2c4a8564e689600769cc349058d1a04f4e6051
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-11 14:15:01 -0700

    Set up signup tests and begin an outline of what to test

--- src/components/SignUp/SignUp.test.js
+++ src/components/SignUp/SignUp.test.js
@@ -1,8 +1,43 @@
 /* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SignUp } from './SignUp';
+import { shallow } from 'enzyme';
 
+describe('SignUp', () => {
+  let wrapper;
+  let mockFn = jest.fn()
 
-it('should exist and match snapshot', () => {
+  beforeEach(() => {
+    wrapper = shallow(<SignUp handleSubmit={mockFn}
+                              handleLogin={mockFn}/>)
+  })
+
+  it('match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
   
-});
+  it('has default state', () => {
+    const defaultState = {
+      name: '',
+      password: '',
+      email: '',
+      error: ''
+    }
+    expect(wrapper.state()).toEqual(defaultState);
+  })
+
+  describe('addUserDatabase', () => {
+    it('calls existingUser', () => {
+
+    })
+
+    it('only calls postNewUser, handleLogin, and handleSubmit if the user exists', () => {
+
+    })
+
+    it('alerts if email already exists', () => {
+
+    })
+  })
+})
